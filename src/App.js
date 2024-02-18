748e5696ee7eeb26d87c9b34c11ae8b2d2d0cf98
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-02-18 13:56:50 -0500

    Fixed ColorForm import

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, { useState } from 'react'
 import ColorBlock from './ColorBlock'
-
+import ColorForm from './ColorForm'
 
 function App() {
-  let [colors, setColors] = useState(['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
+  let [colors, setColors] = useState([
+    'violet',
+    'blue',
+    'lightblue',
+    'green',
+    'greenyellow',
+    'yellow',
+    'orange',
+    'red'
+  ]);
 
   let colorMap = colors.map((color, i) => {
-    return (
-      <ColorBlock key={i} color={color} />
-    )
-  })
+    return <ColorBlock key={i} color={color} />;
+  });
 
   const addColor = (newColor) => {
-    setColors([...colors, newColor])
-}
-  
+    setColors([...colors, newColor]);
+  };
+
   return (
     <div className="App">
       {colorMap}
+      {/* Render ColorForm component and pass addColor function as a prop */}
+      <ColorForm addColor={addColor} />
     </div>
   );
 }
